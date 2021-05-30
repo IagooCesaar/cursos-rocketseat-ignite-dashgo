e77b1b20fd36c2319bb8279d9e461eb355b9255a
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-30 14:24:52 -0300

    criando componente Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { Flex } from '@chakra-ui/react'
+
+export function Header() {
+  return (
+    <Flex
+      as='header'
+      w="100%"
+      maxWidth={1488}
+      h='28'
+      marginX='auto'
+      mt='4'
+      px='6'
+      align='center'
+    >
+      Header
+    </Flex>
+  )
+}
