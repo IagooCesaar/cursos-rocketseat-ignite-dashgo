46f001b83949f0c79a7a18d8bbc03d5fef120d06
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-30 15:23:35 -0300

    criando containers para gráficos

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,9 @@
-import { Flex } from "@chakra-ui/layout";
+import {
+  Flex,
+  SimpleGrid,
+  Box,
+  Text,
+} from "@chakra-ui/react";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 
@@ -10,6 +15,25 @@ export default function Dashboard() {
 
       <Flex width="100%" my="6" maxWidth={1480} mx='auto' px='6'>
         <Sidebar />
+
+        <SimpleGrid flex='1' gap='4' minChildWidth='320px' align='flex-start'>
+          <Box
+            pg='8'
+            bg='gray.800'
+            borderRadius={8}
+          // pb='4'
+          >
+            <Text fontSize='lg' mb='4'>Inscritos da semana</Text>
+          </Box>
+          <Box
+            pg='8'
+            bg='gray.800'
+            borderRadius={8}
+          // pb='4'
+          >
+            <Text fontSize='lg' mb='4'>Taxa de abertura</Text>
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   )
