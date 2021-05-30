f98e65b6b88e62b99347b6b8f20dfeffc1bc974c
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-30 13:26:22 -0300

    criando layout do formulário de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,33 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Input, Button } from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <Flex
-      w='100vw' h='100vh'
+      w='100vw'
+      h='100vh'
       align='center'
       justify='center'
     >
-      <h1>Hello</h1>
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg='gray.800'
+        p='8' // padding 2rem ou  32px (múltiplo 4)
+        borderRadius='8'
+        flexDir="column"
+      >
+        <Input type='email' name='email' />
+        <Input type='password' name='password' />
+
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme='pink'
+        >
+          Entrar
+        </Button>
+      </Flex>
     </Flex>
   )
 }
