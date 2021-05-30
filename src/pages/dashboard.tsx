fbabf1004fa8120c4dcdda1c81f1ecd683eff898
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-30 15:49:59 -0300

    configurando gráfico

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,6 +3,7 @@ import {
   SimpleGrid,
   Box,
   Text,
+  theme,
 } from "@chakra-ui/react";
 import dynamic from 'next/dynamic';
 
@@ -14,7 +15,42 @@ const Chart = dynamic(() => import('react-apexcharts'), {
 })
 
 const options = {
-
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[600],
+    },
+    axisTicks: {
+      color: theme.colors.gray[600],
+    },
+    categories: [
+      '2021-05-30T00:00:00.000Z',
+      '2021-05-31T00:00:00.000Z',
+      '2021-06-01T00:00:00.000Z',
+      '2021-06-02T00:00:00.000Z',
+      '2021-06-03T00:00:00.000Z',
+      '2021-06-04T00:00:00.000Z',
+      '2021-06-05T00:00:00.000Z',
+    ]
+  }
 }
 const series = [
   {
@@ -32,26 +68,22 @@ export default function Dashboard() {
 
         <SimpleGrid flex='1' gap='4' minChildWidth='320px' align='flex-start'>
           <Box
-            pg='8'
+            p='8'
             bg='gray.800'
             borderRadius={8}
-          // pb='4'
+            pb='4'
           >
             <Text fontSize='lg' mb='4'>Inscritos da semana</Text>
-            <Chart
-              type='area'
-              height={160}
-              options={options}
-              series={series}
-            />
+            <Chart options={options} series={series} type='area' height={160} />
           </Box>
           <Box
-            pg='8'
+            p='8'
             bg='gray.800'
             borderRadius={8}
-          // pb='4'
+            pb='4'
           >
             <Text fontSize='lg' mb='4'>Taxa de abertura</Text>
+            <Chart options={options} series={series} type='area' height={160} />
           </Box>
         </SimpleGrid>
       </Flex>
