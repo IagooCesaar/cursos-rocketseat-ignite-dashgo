700087a4a0e98daf53e79ab2267d318ec48f07b8
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-31 12:57:25 -0300

    preparando drawer para Siderbar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,37 @@
 import {
   Box,
+  Drawer,
+  useBreakpointValue,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
 } from "@chakra-ui/react";
 import React from "react";
 
 import { SidebarNav } from "./SidebarNav";
 
 export function Sidebar() {
+  const isDrawerSidebar = useBreakpointValue({
+    base: true,
+    lg: false,
+  })
+  if (isDrawerSidebar) {
+    return (
+      <Drawer isOpen={true} placement='left' onClose={() => { }} >
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerCloseButton mt='6' />
+            <DrawerHeader>Navegação</DrawerHeader>
+            <DrawerBody>
+              <SidebarNav />
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    )
+  }
   return (
     <Box as='aside' w='64' mr='8' >
       <SidebarNav />
