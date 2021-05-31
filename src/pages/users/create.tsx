3786453fbfb0fa55b265e6aa04a1b4dfaf7c0911
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-31 10:06:29 -0300

    estrutura página de criaçaõ de usuários

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,37 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Th,
+  Thead,
+  Tr,
+  Checkbox,
+  Tbody,
+  Td,
+  Text
+} from "@chakra-ui/react";
+import React from "react";
+
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
+import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function CreateUser() {
+  return (
+    <Box>
+      <Header />
+      <Flex width="100%" my="6" maxWidth={1480} mx='auto' px='6'>
+        <Sidebar />
+
+        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
