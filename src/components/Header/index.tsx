6ace68b7a97daa906f861d43de8da91504235498
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-31 13:13:10 -0300

    exibindi botão no Header para controlar Sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,19 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import {
+  Flex,
+  IconButton,
+  useBreakpointValue,
+  Icon
+} from '@chakra-ui/react'
+import { RiMenuLine } from 'react-icons/ri'
+
+import { useSidebarDrawer } from '../../context/SidebarDrawerContext'
 import { Logo } from './Logo'
 import { NotificationsNav } from './NotificationsNav'
 import { Profile } from './Profile'
 import { Searchbox } from './Searchbox'
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -20,6 +29,16 @@ export function Header() {
       px='6'
       align='center'
     >
+      { !isWideVersion && (
+        <IconButton
+          aria-label='Open navigation'
+          icon={<Icon as={RiMenuLine} />}
+          fontSize='24'
+          variant='unstyled'
+          onCLick={onOpen}
+          mr='2'
+        />
+      )}
       <Logo />
       {isWideVersion && <Searchbox />}
       <Flex align="center" ml='auto'>
