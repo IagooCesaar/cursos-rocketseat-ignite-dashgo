d834f6fd57331a3f3743c6a8b0564e95fa3d2566
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-31 10:04:28 -0300

    Melhorando layout e informaçãoes da paginação

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,4 +1,4 @@
-import { Stack, Button } from "@chakra-ui/react";
+import { Stack, Button, Box } from "@chakra-ui/react";
 
 export function Pagination() {
   return (
@@ -9,47 +9,52 @@ export function Pagination() {
       align='center'
       spacing='6'
     >
-      <Button
-        size='sm'
-        fontSize='xs'
-        width='4'
-        colorScheme='pink'
-        disabled
-        _disabled={{
-          bg: 'pink.500',
-          cursor: 'default',
-        }}
-      >1</Button>
+      <Box>
+        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+      </Box>
+      <Stack direction='row' spacing='2' >
+        <Button
+          size='sm'
+          fontSize='xs'
+          width='4'
+          colorScheme='pink'
+          disabled
+          _disabled={{
+            bg: 'pink.500',
+            cursor: 'default',
+          }}
+        >1</Button>
 
-      <Button
-        size='sm'
-        fontSize='xs'
-        width='4'
-        bg='gray.700'
-        _hover={{
-          bg: 'gray.500'
-        }}
-      >2</Button>
+        <Button
+          size='sm'
+          fontSize='xs'
+          width='4'
+          bg='gray.700'
+          _hover={{
+            bg: 'gray.500'
+          }}
+        >2</Button>
 
-      <Button
-        size='sm'
-        fontSize='xs'
-        width='4'
-        bg='gray.700'
-        _hover={{
-          bg: 'gray.500'
-        }}
-      >3</Button>
+        <Button
+          size='sm'
+          fontSize='xs'
+          width='4'
+          bg='gray.700'
+          _hover={{
+            bg: 'gray.500'
+          }}
+        >3</Button>
 
-      <Button
-        size='sm'
-        fontSize='xs'
-        width='4'
-        bg='gray.700'
-        _hover={{
-          bg: 'gray.500'
-        }}
-      >4</Button>
+        <Button
+          size='sm'
+          fontSize='xs'
+          width='4'
+          bg='gray.700'
+          _hover={{
+            bg: 'gray.500'
+          }}
+        >4</Button>
+      </Stack>
 
     </Stack>
   )
