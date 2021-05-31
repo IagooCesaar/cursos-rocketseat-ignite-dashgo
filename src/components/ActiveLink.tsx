57f77ec13494bdb3fd0034cb5c7014e3897f3c8d
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-31 13:50:42 -0300

    lógica de identificação de link ativo

--- src/components/ActiveLink.tsx
+++ src/components/ActiveLink.tsx
@@ -1,4 +1,5 @@
 import Link, { LinkProps } from 'next/link';
+import { useRouter } from 'next/router';
 import { cloneElement, ReactElement } from 'react';
 
 interface ActiveLinkProps extends LinkProps {
@@ -9,7 +10,11 @@ export function ActiveLink({
   children,
   ...rest
 }: ActiveLinkProps) {
-  const isActive = true;
+  const { asPath } = useRouter();
+  let isActive = false;
+  if (asPath === rest.href || asPath === rest.as) {
+    isActive = true;
+  }
   return (
     <Link {...rest}>
       {cloneElement(children, {
