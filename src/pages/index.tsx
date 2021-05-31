37464093d82446f9aaef9776240fddfda51210c3
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-31 14:43:18 -0300

    utilizando React Hook Form no SignIn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,18 @@ import {
   Button,
   Stack,
 } from '@chakra-ui/react'
+import { useForm } from 'react-hook-form';
 
 import { Input } from '../components/Form/Input'
 
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm();
+  const { errors } = formState;
+
+  function handleSignIn(values) {
+    console.log(values);
+  }
+
   return (
     <Flex
       w='100vw'
@@ -22,10 +30,21 @@ export default function SignIn() {
         p='8' // padding 2rem ou  32px (múltiplo 4)
         borderRadius='8'
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing='4'>
-          <Input name='email' type='email' label="E-mail" />
-          <Input name='password' type='password' label="Senha" />
+          <Input
+            name='email'
+            type='email'
+            label="E-mail"
+            {...register('email')}
+          />
+          <Input
+            name='password'
+            type='password'
+            label="Senha"
+            {...register('password')}
+          />
         </Stack>
 
         <Button type="submit" mt="6" colorScheme='pink' size='lg' >
