cb5e2ce7022c6a49781f55c83f7a5f8d10832dcc
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-31 13:37:44 -0300

    Implementando navegação entre páginas

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,25 +1,30 @@
 import {
-  Link,
+  Link as ChakraLink,
   Icon,
   Text,
   LinkProps as ChakraLinkProps
 } from "@chakra-ui/react";
+import Link from 'next/link';
 import { ElementType } from "react";
 
 interface NavLinkProps extends ChakraLinkProps {
   children: string;
   icon: ElementType;
+  href: string;
 }
 
 export function NavLink({
   children,
   icon,
+  href,
   ...rest
 }: NavLinkProps) {
   return (
-    <Link display='flex' align='center' {...rest}>
-      <Icon as={icon} fontSize='20' />
-      <Text ml='4' fontWeight='medium'>{children}</Text>
+    <Link href={href}>
+      <ChakraLink display='flex' align='center' {...rest}>
+        <Icon as={icon} fontSize='20' />
+        <Text ml='4' fontWeight='medium'>{children}</Text>
+      </ChakraLink>
     </Link>
   )
 }
