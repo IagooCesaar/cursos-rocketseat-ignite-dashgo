1cdd14b99a129259dc7d62a9a046ace3755a6966
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-06 14:55:14 -0300

    correção spread operator

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -11,7 +11,7 @@ interface PaginationProps {
 const siblingsCount = 1;
 
 function generatePagesArray(from: number, to: number) {
-  return [new Array(to - from)].map((_, index) => {
+  return [...new Array(to - from)].map((_, index) => {
     return from + index + 1;
   }).filter(page => page > 0)
 }
