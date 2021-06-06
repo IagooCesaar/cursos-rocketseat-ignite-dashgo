b9bc534b775147152613d76974c6987d15a1e4c4
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-06 09:26:29 -0300

    criando hook próprio para lista de usuários

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -17,32 +17,16 @@ import {
 } from "@chakra-ui/react";
 import Link from "next/link";
 import React, { useEffect } from "react";
-import { useQuery } from 'react-query'
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
-import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, error, isFetching } = useQuery('users', async () => {
-    const { data } = await api.get('/users');
-    const users = data.users.map(user => {
-      return {
-        ...user,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    });
-    return users;
-  }, {
-    staleTime: 1000 * 5, // 5 segundos
-  })
+  const { data, isLoading, error, isFetching } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
