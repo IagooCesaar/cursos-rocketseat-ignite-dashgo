9cc3c10df61cc4f29ff474abe5e8a94eeeea4e81
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-06 17:12:22 -0300

    enviando dados para api

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -60,9 +60,7 @@ export default function CreateUser() {
   const { errors, isSubmitting } = formState;
 
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (data) => {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-
-    console.log(data)
+    await createUser.mutateAsync(data)
   }
 
   return (
