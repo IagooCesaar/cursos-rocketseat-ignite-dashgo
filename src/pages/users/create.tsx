105945131aea52a8f90c726ca6466aabdae7e317
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-06 17:08:00 -0300

    função para criar novos usuários

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -13,10 +13,12 @@ import React from "react";
 import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup'
+import { useMutation } from 'react-query';
 
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
 
 type CreateUserFormData = {
   name: string;
@@ -42,6 +44,16 @@ const createUserFormSchema = yup.object().shape({
 })
 
 export default function CreateUser() {
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('/users', {
+      user: {
+        ...user,
+        created_at: new Date(),
+      }
+    })
+    return response.data.user;
+  });
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema)
   })
