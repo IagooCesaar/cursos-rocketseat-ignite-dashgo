6641e63dd39fb4350d02ec0ec78372e04fe8efeb
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-06 14:08:08 -0300

    desacoplando função para obter lista d eusuários

--- src/services/hooks/useUsers.tsx
+++ src/services/hooks/useUsers.tsx
@@ -1,21 +1,23 @@
 import { useQuery } from "react-query";
 import { api } from "../api";
 
+export async function getUsers() {
+  const { data } = await api.get('/users');
+  const users = data.users.map(user => {
+    return {
+      ...user,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  });
+  return users;
+}
+
 export function useUsers() {
-  return useQuery('users', async () => {
-    const { data } = await api.get('/users');
-    const users = data.users.map(user => {
-      return {
-        ...user,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    });
-    return users;
-  }, {
+  return useQuery('users', getUsers, {
     staleTime: 1000 * 5, // 5 segundos
   })
 }
